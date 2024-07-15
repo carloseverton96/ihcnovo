7dfed78a040a89815e7692d0e881e7d9c65ee455
Éverton Alves <140563844+carloseverton96@users.noreply.github.com>
2024-07-15 11:09:42 -0300

    sd

--- visa/client/src/components/dialog/FormDialogPessoaFisica.js
+++ visa/client/src/components/dialog/FormDialogPessoaFisica.js
@@ -1,3 +1,5 @@
+// src/components/FormDialogPessoaFisica.js
+
 import React, { useState, useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
@@ -7,7 +9,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Axios from "axios";
 
-const FormDialogPessoaFisica = (props) => {
+export default function FormDialogPessoaFisica(props) {
   const [editValues, setEditValues] = useState({
     id_pessoa: props.id_pessoa || "",
     nome: props.nome || "",
@@ -36,126 +38,143 @@ const FormDialogPessoaFisica = (props) => {
     cid10: props.cid10 || "",
     datainclusao: props.datainclusao || "",
     datadesligamento: props.datadesligamento || "",
-    usoimagem: props.usoimagem || "",
+    usodeimagem: props.usodeimagem || "",
   });
 
   useEffect(() => {
     if (props.id_pessoa !== undefined && props.id_pessoa !== editValues.id_pessoa) {
-      setEditValues({
-        ...editValues,
+      setEditValues((prevValues) => ({
+        ...prevValues,
         id_pessoa: props.id_pessoa,
-      });
+      }));
     }
-  }, [props.id_pessoa]);
+  }, [props.id_pessoa, editValues.id_pessoa]);
 
-  const handleFieldChange = (event) => {
-    const { id, value } = event.target;
-    setEditValues({
-      ...editValues,
-      [id]: value,
-    });
+  const handleChangeValues = (event) => {
+    setEditValues((prevValues) => ({
+      ...prevValues,
+      [event.target.id]: event.target.value,
+    }));
   };
 
   const handleClose = () => {
     props.setOpen(false);
   };
 
-  const handleEditPessoaFisica = () => {
+  const handleEditPessoafisica = () => {
     if (!editValues.id_pessoa) {
       alert("Erro: id_pessoa está faltando nos valores de edição!");
       return;
     }
 
-    const url = `http://localhost:3001/pessoa-fisica/edit/${editValues.id_pessoa}`;
-    Axios.put(url, editValues)
-      .then(() => {
-        const updatedListPessoaFisica = props.listPessoaFisica.map((value) => {
-          if (value.id_pessoa === editValues.id_pessoa) {
-            return { ...editValues };
-          }
+    Axios.put("http://localhost:3001/edit", {
+      id_pessoa: editValues.id_pessoa,
+      nome: editValues.nome,
+      apelido: editValues.apelido,
+      sexo: editValues.sexo,
+      datanascimento: editValues.datanascimento,
+      rg: editValues.rg,
+      ssp: editValues.ssp,
+      cpf: editValues.cpf,
+      cartaosus: editValues.cartaosus,
+      endereco: editValues.endereco,
+      numero: editValues.numero,
+      bairro: editValues.bairro,
+      complemento: editValues.complemento,
+      municipio: editValues.municipio,
+      pontodereferencia: editValues.pontodereferencia,
+      contato1: editValues.contato1,
+      contato2: editValues.contato2,
+      pai: editValues.pai,
+      mae: editValues.mae,
+      responsavel: editValues.responsavel,
+      bolsafamilia: editValues.bolsafamilia,
+      valorbolsafamilia: editValues.valorbolsafamilia,
+      beneficiodeprestacaocontinuada: editValues.beneficiodeprestacaocontinuada,
+      nis: editValues.nis,
+      cid10: editValues.cid10,
+      datainclusao: editValues.datainclusao,
+      datadesligamento: editValues.datadesligamento,
+      usodeimagem: editValues.usodeimagem,
+    }).then(() => {
+      const updatedListPessoaFisica = props.listPessoaFisica.map((value) => {
+        if (value.id_pessoa === editValues.id_pessoa) {
+          return { ...editValues };
+        } else {
           return value;
-        });
-        props.setListPessoaFisica(updatedListPessoaFisica);
-      })
-      .catch((error) => {
-        console.error("Ocorreu um erro ao atualizar a pessoa!", error);
+        }
       });
+
+      props.setListPessoaFisica(updatedListPessoaFisica);
+    });
 
     handleClose();
   };
 
-  const handleDeletePessoaFisica = () => {
-    Axios.delete(`http://localhost:3001/pessoa-fisica/delete/${editValues.id_pessoa}`)
-      .then(() => {
-        props.setListPessoaFisica(
-          props.listPessoaFisica.filter((value) => value.id_pessoa !== editValues.id_pessoa)
-        );
-      })
-      .catch((error) => {
-        console.error("Ocorreu um erro ao excluir a pessoa!", error);
-      });
+  const handleDeletePessoafisica = () => {
+    Axios.delete(`http://localhost:3001/delete/${editValues.id_pessoa}`).then(() => {
+      props.setListPessoaFisica(
+        props.listPessoaFisica.filter((value) => value.id_pessoa !== editValues.id_pessoa)
+      );
+    });
 
     handleClose();
   };
 
   return (
-    <div>
-      <Dialog open={props.open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Editar</DialogTitle>
-        <DialogContent>
+    <Dialog open={props.open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Editar</DialogTitle>
+      <DialogContent>
         <TextField
-            disabled
-            margin="dense"
-            id="id_pessoa"
-            label="ID"
-            value={editValues.id_pessoa}
-            type="text"
-            fullWidth
-          />
-          <TextField
-            
-            margin="dense"
-            id="nome"
-            label="Nome"
-            value={editValues.nome}
-            onChange={handleFieldChange}
-            type="text"
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            id="apelido"
-            label="Apelido"
-            value={editValues.apelido}
-            onChange={handleFieldChange}
-            type="text"
-            fullWidth
-          />
-          <TextField
-            margin="dense"
-            id="sexo"
-            label="Sexo"
-            value={editValues.sexo}
-            onChange={handleFieldChange}
-            type="text"
-            fullWidth
-          />
-          {/* Adicione os outros campos de TextField aqui conforme necessário */}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancelar
-          </Button>
-          <Button onClick={handleDeletePessoaFisica} color="primary">
-            Excluir
-          </Button>
-          <Button onClick={handleEditPessoaFisica} color="primary">
-            Salvar
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
-};
+          disabled
+          margin="dense"
+          id="id_pessoa"
+          label="ID"
+          value={editValues.id_pessoa}
+          type="text"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          id="nome"
+          label="Nome"
+          value={editValues.nome}
+          onChange={handleChangeValues}
+          type="text"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          id="apelido"
+          label="Apelido"
+          value={editValues.apelido}
+          onChange={handleChangeValues}
+          type="text"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          id="sexo"
+          label="Sexo"
+          value={editValues.sexo}
+          onChange={handleChangeValues}
+          type="text"
+          fullWidth
+        />
 
-export default FormDialogPessoaFisica;
+        {/* Add other TextField components as needed */}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="primary">
+          Cancelar
+        </Button>
+        <Button color="primary" onClick={handleDeletePessoafisica}>
+          Excluir
+        </Button>
+        <Button color="primary" onClick={handleEditPessoafisica}>
+          Salvar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
